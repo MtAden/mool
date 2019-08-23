e78b225b2dd8c440d11793e4d4731695ab2a456a
Alok Choudhary <alok.c83@gmail.com>
2019-08-23 14:29:48 -0500

    First check in

---
+++ Mool/Mool.h
@@ -0,0 +1,19 @@
+//
+//  Mool.h
+//  Mool
+//
+//  Created by Alok Choudhary on 8/23/19.
+//  Copyright © 2019 Mt Aden LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mool.
+FOUNDATION_EXPORT double MoolVersionNumber;
+
+//! Project version string for Mool.
+FOUNDATION_EXPORT const unsigned char MoolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mool/PublicHeader.h>
+
+
---
+++ Mool/UiButton+extensions/UIButton+animation.swift
@@ -0,0 +1,74 @@
+//
+//  UIButton+animation.swift
+//  Mool
+//
+//  Created by Alok Choudhary on 8/23/19.
+//  Copyright © 2019 Mt Aden LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func pulse() {
+        let bounce = CASpringAnimation(keyPath: "transform.scale")
+        bounce.duration = 0.3
+        bounce.fromValue = 0.95
+        bounce.toValue = 1.2
+        bounce.autoreverses = true
+        bounce.repeatCount = 1
+        bounce.initialVelocity = 0.5
+        bounce.damping = 1.0
+        
+        layer.add(bounce, forKey: nil)
+    }
+    
+    func flash() {
+        let flash = CABasicAnimation(keyPath: "opacity")
+        flash.duration = 0.3
+        flash.fromValue = 1
+        flash.toValue = 0.1
+        flash.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        flash.autoreverses = true
+        flash.repeatCount = 2
+        
+        layer.add(flash, forKey: nil)
+    }
+    
+    func nopeShake() {
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.1
+        shake.repeatCount = 2
+        shake.autoreverses = true
+        
+        let fromPoint = CGPoint(x: center.x - 5, y: center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        
+        let toPoint = CGPoint(x: center.x + 5, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        
+        layer.add(shake, forKey: nil)
+    }
+    
+    func bounce() {
+        let bounce = CABasicAnimation(keyPath: "position")
+        bounce.duration = 0.1
+        bounce.repeatCount = 2
+        bounce.autoreverses = true
+        
+        let fromPoint = CGPoint(x: center.x, y: center.y - 5)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        
+        let toPoint = CGPoint(x: center.x, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        
+        bounce.fromValue = fromValue
+        bounce.toValue = toValue
+        
+        layer.add(bounce, forKey: nil)
+    }
+}
+
